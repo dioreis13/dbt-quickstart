962f2e5886696130ab0682d6c3c0288f152106ee
diogo.c.reis@aubay.pt <diogo.c.reis@aubay.pt>
2025-02-10 15:39:26 +0000

    answers adjustment

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,20 +1,30 @@
-with orders as (
-    select * from {{ ref("stg_jaffle_shop__orders")}}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
 
 payments as (
-    select * from {{ref("stg_stripe__payments")}}
+    select * from {{ ref ('stg_stripe__payments') }}
 ),
 
-final as (
-    select 
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
         orders.order_id,
         orders.customer_id,
-        payments.amount
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
 
-    from orders 
-    left join payments on order_id = payment_id 
-
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from final

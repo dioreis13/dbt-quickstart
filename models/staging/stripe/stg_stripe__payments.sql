de829bfff5f3a0f87a7d3aec6bd7f436433fea60
Diogo Reis <diogo.c.reis@aubay.pt>
2025-02-24 21:43:14 +0000

    Demo until Jinja and Macros

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,10 +1,26 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+with
 
-from {{ source('stripe', 'payment') }}
+source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+transformed as (
+
+    select 
+
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        -- amount is stored in cents, convert it to dollars
+        --round(amount / 100.0, 2) as payment_amount,
+        {{ cents_to_dollars('payment_amount',4)}} as payment_amount,
+        created as payment_created_at
+
+    from source
+
+)
+
+select * from transformed

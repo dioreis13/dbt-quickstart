931d032f1f95ebd981bcdd4484a4cb55617e8d5f
dioreis13 <diogocol@gmail.com>
2025-02-27 00:14:12 +0100

    Refactoring legacy code (#4)
    * Demo until Jinja and Macros
    
    * updatind packages and using packages and models
    
    * Macros, Jinja and Packages demos
    
    * exposures demo additions and cleanup of snowflake_spend package
    
    ---------
    
    Co-authored-by: Diogo Reis <diogo.c.reis@aubay.pt>

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -16,7 +16,7 @@ transformed as (
         status as payment_status,
         -- amount is stored in cents, convert it to dollars
         --round(amount / 100.0, 2) as payment_amount,
-        {{ cents_to_dollars('payment_amount',4)}} as payment_amount,
+        {{ cents_to_dollars('amount',4)}} as payment_amount,
         created as payment_created_at
 
     from source
